f926b88df69857dac3af0dc22d8c4dd657ce51fa
Hunter Hutchisson <hunter.hutchisson@gmail.com>
2021-10-22 13:37:38 -0700

    album page displays albums

--- routes/albums.js
+++ routes/albums.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const router = express.Router();
 let dataFile = require('../data/data.json');
+let albumsArr = dataFile.albums
 
 
 router.get('/albums', (req, res) => {
-    res.render('albums')
+    let albumPhotos = []
+
+    albumsArr.forEach(albumObj => {
+        albumPhotos = albumPhotos.concat(albumObj.artwork)
+    })
+    res.render('albums', {
+        artwork: albumPhotos,
+        albumsArr: albumsArr,
+        navAlbums: albumsArr,
+        pageTitle: "Modest Moust -- Albums"
+    })
+
+})
+
+router.get('/albums/:albumid', (req, res) => {
+    //display 1 album
+    let album = req.params.albumid;
+    let photo = [];
+    let albums = [];
+
+    albumsArr.forEach(albumObj => {
+        if(albumObj.shortname === album){
+            photo = [...albumObj.artwork]
+            albums.push(albumObj)
+        }
+    })
+
+    res.render('albums', {
+        artwork: photo,
+        navAlbums: albumsArr,
+        albumsArr: albums,
+        pageTitle: `Modest Mouse -- ${albums[0].albumName}`
+    })
 })
 
 module.exports = router;
